0c15de8b9bf79f4768c32d6bbc7fbec0b9e76833
Christopher Gruber <gruber.christopher@gmail.com>
2014-04-08 22:34:25 -0500

    Project creation

---
+++ ShowInstalledFonts/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShowInstalledFonts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.FontListBox.ItemsSource = Fonts.SystemFontFamilies.OrderBy(x => x.Source);
+        }
+
+        private void FontListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.DemoFontTextBox.FontFamily = this.FontListBox.SelectedItem as FontFamily;
+        }
+    }
+}
